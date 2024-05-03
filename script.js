09d1bb6e4f75ed805209262acf74557f69ab3557
David Lopes <106824008+DavidLBO@users.noreply.github.com>
2024-05-02 22:04:08 -0300

    Criando carrinho de produtos

---
+++ script.js
@@ -0,0 +1,24 @@
+let carrinho = [];
+
+function update_display() {
+  let display_carrinho = document.getElementById('cart-Display');
+  display_carrinho.innerHTML = `
+      ${carrinho.map(item => `
+        <div>
+          <h1>${item.nome}</h1>
+          <p>Descrição</p>
+          <p>Valor: ${item.preco.toFixed(2)}</p>
+        </div>
+      `).join("")}
+    `;
+}
+
+function adicionar_carrinho(nome_produto, preco_produto) {
+  let produtos = {
+    nome: nome_produto,
+    preco: preco_produto
+  };
+  carrinho.push(produtos);
+  update_display();
+}
+
